ae690846e1176891760683b6252eb0dc1fddb9ab
MelancholicHound <furyx.resonance@gmail.com>
2024-08-28 08:30:39 +0800

    initial components

---
+++ frontend/src/app/devices/printer/printer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-printer',
+  standalone: true,
+  imports: [],
+  templateUrl: './printer.component.html',
+  styleUrl: './printer.component.scss'
+})
+export class PrinterComponent {
+
+}
