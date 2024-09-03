be5dc55164d7ef54386984311b46fa42101b35f2
MelancholicHound <furyx.resonance@gmail.com>
2024-09-03 10:55:47 +0800

    batch form ready

--- frontend/src/app/forms/batch/batch.component.ts
+++ frontend/src/app/forms/batch/batch.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Validators, FormGroup, FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-batch',
   standalone: true,
-  imports: [],
+  imports: [
+      ReactiveFormsModule,
+      FormsModule
+  ],
   templateUrl: './batch.component.html',
   styleUrl: './batch.component.scss'
 })
 export class BatchComponent {
+    @Output() booleanEvent = new EventEmitter<boolean>();
+    @Output() isAddingBatch = new EventEmitter<boolean>();
 
+    batchForm!: FormGroup;
+
+    constructor(private router: Router) { }
+
+    createBatchFormGroup(): FormGroup {
+        return new FormGroup({
+            supplierId: new FormControl('', [Validators.required]),
+            serviceCenter: new FormControl('', [Validators.required]),
+            dateDelivered: new FormControl('', [Validators.required]),
+            validUntil: new FormControl('', [Validators.required]),
+            dateTested: new FormControl('', [Validators.required]),
+        });
+    }
+
+    returnToggle() {
+        this.booleanEvent.emit(true);
+    }
+
+    addBatch() {
+        this.isAddingBatch.emit(true);
+    }
 }
