d7be0982b0016a906f893085fba2e7798a81be7a
MelancholicHound <furyx.resonance@gmail.com>
2024-09-09 11:29:48 +0800

    add device done

--- frontend/src/app/pages/add-device/add-device.component.ts
+++ frontend/src/app/pages/add-device/add-device.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { PeripheralsComponent } from '../../components/peripherals/peripherals.component';
+import { ConnectionsComponent } from '../../components/connections/connections.component';
+import { SoftwaresComponent } from '../../components/softwares/softwares.component';
 
 @Component({
-  selector: 'app-add-device',
-  standalone: true,
-  imports: [],
-  templateUrl: './add-device.component.html',
-  styleUrl: './add-device.component.scss'
+    selector: 'app-add-device',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        PeripheralsComponent,
+        ConnectionsComponent,
+        SoftwaresComponent
+    ],
+    templateUrl: './add-device.component.html',
+    styleUrl: './add-device.component.scss'
 })
-export class AddDeviceComponent {
+export class AddDeviceComponent implements OnInit {
+    selected: any;
 
+    ngOnInit(): void {
+        this.selected = localStorage.getItem('device');
+        localStorage.removeItem('device');
+    }
+
+    backButton() {
+        const toAddBatch = document.getElementById('add-batch') as HTMLAnchorElement;
+        toAddBatch.click();
+    }
 }
