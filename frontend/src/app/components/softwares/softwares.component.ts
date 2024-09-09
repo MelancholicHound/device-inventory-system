ad2777e9666458a7b60ca88ff2299f3030732f2e
MelancholicHound <furyx.resonance@gmail.com>
2024-09-09 13:58:52 +0800

    softwares ready

--- frontend/src/app/components/softwares/softwares.component.ts
+++ frontend/src/app/components/softwares/softwares.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { ParamsService } from '../../util/services/params.service';
 
 @Component({
   selector: 'app-softwares',
   standalone: true,
-  imports: [],
+  imports: [
+      NgFor
+  ],
+  providers: [
+      ParamsService
+  ],
   templateUrl: './softwares.component.html',
   styleUrl: './softwares.component.scss'
 })
-export class SoftwaresComponent {
+export class SoftwaresComponent implements OnInit {
+    fetchedOS!: any; fetchedSecurity!: any; fetchedProdTool!: any;
 
+    constructor(private params: ParamsService) { }
+
+    ngOnInit(): void {
+        this.params.getOS().subscribe(res => this.fetchedOS = res);
+        this.params.getSecurity().subscribe(res => this.fetchedSecurity = res);
+        this.params.getProdTools().subscribe(res => this.fetchedProdTool = res);
+    }
 }
