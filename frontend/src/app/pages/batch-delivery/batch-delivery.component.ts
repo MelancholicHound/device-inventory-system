aadc983e6b62a3ed7e01c74dc8e500b6831a9119
MelancholicHound <furyx.resonance@gmail.com>
2024-09-09 08:51:38 +0800

    .

--- frontend/src/app/pages/batch-delivery/batch-delivery.component.ts
+++ frontend/src/app/pages/batch-delivery/batch-delivery.component.ts
@@ -1,12 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { BatchComponent } from '../../forms/batch/batch.component';
+import { SupplierComponent } from '../../forms/supplier/supplier.component';
+
+export interface BatchTable {
+    formattedId: string;
+    supplier: string;
+    dateDelivered: string;
+    validUntil: string;
+}
 
 @Component({
-  selector: 'app-batch-delivery',
-  standalone: true,
-  imports: [],
-  templateUrl: './batch-delivery.component.html',
-  styleUrl: './batch-delivery.component.scss'
+    selector: 'app-batch-delivery',
+    standalone: true,
+    imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
+        BatchComponent,
+        SupplierComponent,
+        NgIf
+    ],
+    templateUrl: './batch-delivery.component.html',
+    styleUrl: './batch-delivery.component.scss'
 })
-export class BatchDeliveryComponent {
 
+export class BatchDeliveryComponent implements AfterViewInit, OnInit {
+    displayedColumns: string[] = ['formattedId', 'supplier', 'dateDelivered', 'validUntil', 'settings'];
+    dataSource!: MatTableDataSource<BatchTable>;
+
+    fetchedData!: any;
+
+    toggleBatchForm: boolean = true; toggleSupplierForm: boolean = false;
+
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
+
+    constructor(private router: Router) {
+        this.dataSource = new MatTableDataSource(this.fetchedData);
+    }
+
+    ngAfterViewInit(): void {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+    }
+
+    ngOnInit(): void {
+        var addBatchModal = document.getElementById('add-batch') as HTMLDivElement;
+        var openModal = document.getElementById('open-add-batch') as HTMLButtonElement;
+        var closeModal = document.getElementById('close-add-batch') as HTMLButtonElement;
+
+        openModal.onclick = function() {
+            addBatchModal.style.display = 'block';
+        }
+
+        closeModal.onclick = function() {
+            addBatchModal.style.display = 'none';
+        }
+    }
+
+    toggleBatch(value: boolean): void {
+        this.toggleBatchForm = value;
+        this.toggleSupplierForm = !value;
+    }
+
+    toggleSupplier(value: boolean): void {
+        this.toggleBatchForm = !value;
+        this.toggleSupplierForm = value;
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+
+        if (this.dataSource.paginator) {
+            this.dataSource.paginator.firstPage();
+        }
+    }
+
+    isAdding(item: boolean) {
+        if (item) {
+            var closeModal = document.getElementById('close-add-batch') as HTMLButtonElement;
+            closeModal.click();
+            this.router.navigate(['add-batch'], { queryParams: { branch: new Date().getTime() } });
+        }
+    }
 }
