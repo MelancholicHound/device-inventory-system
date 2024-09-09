7ef949dd6c79ccece8facbfe2cae0ac7b4ec4be3
MelancholicHound <furyx.resonance@gmail.com>
2024-09-09 13:45:58 +0800

    peripherals ready

--- frontend/src/app/components/connections/connections.component.ts
+++ frontend/src/app/components/connections/connections.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { ParamsService } from '../../util/services/params.service';
 
 @Component({
-  selector: 'app-connections',
-  standalone: true,
-  imports: [],
-  templateUrl: './connections.component.html',
-  styleUrl: './connections.component.scss'
+    selector: 'app-connections',
+    standalone: true,
+    imports: [
+        NgFor
+    ],
+    providers: [
+        ParamsService
+    ],
+    templateUrl: './connections.component.html',
+    styleUrl: './connections.component.scss'
 })
-export class ConnectionsComponent {
+export class ConnectionsComponent implements OnInit {
+    fetchedConnections!: any;
 
+    constructor(private params: ParamsService) { }
+
+    ngOnInit(): void {
+        this.params.getConnections().subscribe(res => this.fetchedConnections = res);
+    }
 }
--- frontend/src/app/components/peripherals/peripherals.component.ts
+++ frontend/src/app/components/peripherals/peripherals.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { ParamsService } from '../../util/services/params.service';
 
 @Component({
-  selector: 'app-peripherals',
-  standalone: true,
-  imports: [],
-  templateUrl: './peripherals.component.html',
-  styleUrl: './peripherals.component.scss'
+    selector: 'app-peripherals',
+    standalone: true,
+    imports: [
+        NgFor
+    ],
+    providers: [
+        ParamsService
+    ],
+    templateUrl: './peripherals.component.html',
+    styleUrl: './peripherals.component.scss'
 })
-export class PeripheralsComponent {
+export class PeripheralsComponent implements OnInit {
+    fetchedUPSBrand!: any;
 
+    constructor(private params: ParamsService) { }
+
+    ngOnInit(): void {
+        this.params.getUPSBrand().subscribe(res => this.fetchedUPSBrand = res);
+    }
 }
