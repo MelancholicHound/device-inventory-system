0eb7554cb7274df3d228d5413e17303ac803e40c
MelancholicHound <furyx.resonance@gmail.com>
2025-05-22 16:33:09 +0800

    .

--- dis-backend/util/database.device-aio.js
+++ dis-backend/util/database.device-aio.js
@@ -2,6 +2,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const { Section, Batch } = require('../util/database.user');
 const { UPS } = require('../util/database.device-ups');
+const { RAM, Storage } = require('../util/database.capacities');
 const { BrandAIO } = require('../util/database.brands');
 
 require('dotenv').config();
@@ -26,18 +27,15 @@ const AIO = sequelize.define('tbl_device_aio', {
             key: 'id'
         }
     },
+    device_tag: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
     section_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: {
             model: Section,
-            key: 'id'
-        }
-    },
-    ups_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: UPS,
             key: 'id'
         }
     },
@@ -49,9 +47,58 @@ const AIO = sequelize.define('tbl_device_aio', {
             key: 'id'
         }
     },
+    model: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
     serial_number: {
         type: DataTypes.STRING,
         unique: true
+    },
+    ups_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: UPS,
+            key: 'id'
+        }
+    }
+});
+
+const RAMAIO = sequelize.define('tbl_ram_aio', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: AIO,
+            key: 'id'
+        }
+    },
+    aio_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: RAM,
+            key: 'id'
+        }
+    }
+});
+
+const StorageAIO = sequelize.define('tbl_storage_aio', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: AIO,
+            key: 'id'
+        }
+    },
+    storage_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Storage,
+            key: 'id'
+        }
     }
 });
 
@@ -59,5 +106,7 @@ sequelize.sync({ alter: true })
 .catch((error) => console.log('Error creating table: ', error));
 
 module.exports = {
+    RAMAIO,
+    StorageAIO,
     AIO
 }
